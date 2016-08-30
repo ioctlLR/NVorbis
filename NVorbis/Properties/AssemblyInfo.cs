4c232e760fa46a296bdb2de4654c51b7e91f4c60
SomeAnon42 <goldenjoe@outlook.com>
2016-08-30 19:06:02 +0300

    Added support for .NET Core & UWP (by transitioning to .NETStandard & NuGet 3.5)

--- NVorbis/Properties/AssemblyInfo.cs
+++ NVorbis/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Resources;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NVorbis.Portable")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NVorbis")] 
+[assembly: AssemblyDescription("A Vorbis decoder in C#")] 
+[assembly: AssemblyCompany("Andrew Ward")] 
+[assembly: AssemblyProduct("NVorbis")] 
+[assembly: AssemblyCopyright("Copyright © Andrew Ward 2014")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NVorbis.Portable")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -24,5 +25,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)] 
+[assembly: AssemblyVersion("0.8.4.0")] 
+[assembly: AssemblyFileVersion("0.8.4.0")]
